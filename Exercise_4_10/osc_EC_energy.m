8f08706d99825097dd5a8aab6194e73b3afbec6b
PetLak <petr.p.lakomy@student.lab.fi>
2020-03-09 20:06:24 +0200

    This function for computes ODEs by using Euler-Cromer scheme was added. The function was copied from the book - Programming for Computations MATLAB/Octave.

---
+++ Exercise_4_10/osc_EC_energy.m
@@ -0,0 +1,21 @@
+% Euler-Cromer scheme
+omega = 2;
+P = 2*pi/omega;
+dt = P/80;
+T = 3*P;
+N_t = floor(round(T/dt));
+t = linspace(0, N_t*dt, N_t+1);
+
+u = zeros(N_t+1, 1);
+v = zeros(N_t+1, 1);
+
+% Initial condition
+X_0 = 2;
+u(1) = X_0;
+v(1) = 0;
+
+% Step equations forward in time
+for n = 1:N_t
+    v(n+1) = v(n) - dt*omega^2*u(n);
+    u(n+1) = u(n) + dt*v(n+1);
+end
