88d75cb9cc3b50714f3d83e3bbb75e577a7ecd04
PetLak <petr.p.lakomy@student.lab.fi>
2020-03-10 20:00:01 +0200

    For loop was implemented, from 1 to length of "dt" vector.
    Velocity and position were modified to be matrix with column equal to length of "dt" vector...

--- Exercise_4_14/osc_BE.m
+++ Exercise_4_14/osc_BE.m
@@ -2,27 +2,32 @@ omega = 2;
 P = 2*pi/omega;
 dt = [P/20 P/2000]';
 T = 3*P;
-N_t = floor(round(T/dt(1)));
-t = linspace(0, N_t*dt(1), N_t+1);
-
-u = zeros(N_t+1, 1);
-v = zeros(N_t+1, 1);
 
 % Initial condition
 X_0 = 2;
 v_0 = 0;
 
-% Initial condition are assigned.
-u(1) = X_0;
-v(1) = v_0;
+for ii = 1:length(dt)
+N_t = floor(round(T/dt(ii)));
 
-for n = 2:N_t+1
-    u(n) = (1.0/(1+(dt(1)*omega)^2)) * (dt(1)*v(n-1) + u(n-1));
-    v(n) = (1.0/(1+(dt(1)*omega)^2)) * (-dt(1)*omega^2*u(n-1) + v(n-1));
+t = linspace(0, N_t*dt(ii), N_t+1);
+
+u = zeros(N_t+1, length(dt));
+v = zeros(N_t+1, length(dt));
+
+% Initial condition are assigned.
+u(1,:) = X_0;
+v(1,:) = v_0;
+
+    for n = 2:N_t+1
+        u(n,ii) = (1.0/(1+(dt(ii)*omega)^2)) * (dt(ii)*v(n-1,ii) + u(n-1,ii));
+        v(n,ii) = (1.0/(1+(dt(ii)*omega)^2)) * (-dt(ii)*omega^2*u(n-1,ii) + v(n-1,ii));
+    end
+    
+hold on
+plot(t, u(:,ii), 'b--');
 end
 
-plot(t, u, 'b--')
-hold on
 plot(t, X_0*cos(omega*t), 'g-');
 
 legend('numerical', 'exact');
