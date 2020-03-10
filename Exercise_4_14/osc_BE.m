4d90264a4218becdb3b12ed19828614bf4b6631f
PetLak <petr.p.lakomy@student.lab.fi>
2020-03-11 00:59:26 +0200

    Plot was tuned. Legend provides used time step sizes.

--- Exercise_4_14/osc_BE.m
+++ Exercise_4_14/osc_BE.m
@@ -1,3 +1,4 @@
+% Backward Euler scheme 
 omega = 2;
 P = 2*pi/omega;
 dt = [P/20 P/2000]';
@@ -9,7 +10,6 @@ v_0 = 0;
 
 for ii = 1:length(dt)
 N_t = floor(round(T/dt(ii)));
-
 t = linspace(0, N_t*dt(ii), N_t+1);
 
 u = zeros(N_t+1, length(dt));
@@ -23,14 +23,20 @@ v(1,:) = v_0;
         u(n,ii) = (1.0/(1+(dt(ii)*omega)^2)) * (dt(ii)*v(n-1,ii) + u(n-1,ii));
         v(n,ii) = (1.0/(1+(dt(ii)*omega)^2)) * (-dt(ii)*omega^2*u(n-1,ii) + v(n-1,ii));
     end
-    
+
 hold on
-plot(t, u(:,ii), '--');
+plot(t, u(:,ii), '--', 'LineWidth', 1.4);
+
+% filename = strcat('Backward_Euler_dt=', num2str(dt(ii)),'.pdf');
+% print(filename, '-dpdf');  print(filename, '-dpng');
 end
 
-plot(t, X_0*cos(omega*t), 'g-');
+plot(t, X_0*cos(omega*t), 'g-'); % Exact solution for comparison.
 
-legend('numerical1', 'numerical2', 'exact');
+leg_dt1= strcat('dt=', num2str(dt(1)));
+leg_dt2= strcat('dt=', num2str(dt(2)));
+legend(leg_dt1,leg_dt2,'Exact')
+
 title('Backward Euler scheme')
 xlabel('t'); ylabel('Position of mass u');
 
